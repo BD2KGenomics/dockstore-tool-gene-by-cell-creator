dc5001d71c2a1627b7a775a2371eb03820f750db
Brian O'Connor <briandoconnor@gmail.com>
2017-05-11 17:57:58 -0700

    adding some code to deal with FAIL, probably don't want this in the matrix since not sure if these are even really failed

--- src/create_gene_by_cell.py
+++ src/create_gene_by_cell.py
@@ -150,13 +150,16 @@ class GeneByCellCreator:
                 subprocess.check_call(['tar', 'xvf', filepath, '-C', untar_location], stdout=FNULL)
                 #verify success
                 expected_output_dir = filepath.rstrip(GeneByCellCreator.INPUT_FILETYPE)
-                if not os.path.isdir(expected_output_dir):
-                    (base_path, dir_name) = os.path.split(expected_output_dir)
-                    expected_output_dir = os.path.join(base_path, "FAIL."+dir_name)
-                    if not os.path.isdir(expected_output_dir):
-                        raise Exception("After untarring %s, expected output location was not found: %s" % (filepath, expected_output_dir))
                 #get uuid
                 uuid = os.path.basename(expected_output_dir)
+                if not os.path.isdir(expected_output_dir):
+                    (base_path, dir_name) = os.path.split(expected_output_dir)
+                    # this is named FAIL when Holly's QC program determines a fail, ignoring it since not sure if applicable to scRNA-Seq
+                    expected_output_dir = os.path.join(base_path, "FAIL."+dir_name)
+                    # change the uuid to the name without FAIL
+                    uuid = dir_name
+                    if not os.path.isdir(expected_output_dir):
+                        raise Exception("After untarring %s, expected output location was not found: %s" % (filepath, expected_output_dir))
                 if uuid_to_file.has_key(uuid):
                     raise Exception("UUID %s found twice" % uuid)
                 #save and continue
